770804dc181e7f5b8a22841de12d7337cd786156
Anusha <anusha.amin03@gmail.com>
2023-07-07 18:42:59 +0500

    i want to add these files

---
+++ App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Button, SafeAreaView, Text, StyleSheet, Image, TouchableOpacity, AsyncStorage} from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+
+function App() {
+  return (
+    <SafeAreaView>
+      <TextInput placeholder='Enter your name'/>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+
+}
+)
+export default App;
