11f16cd6dd90d89effaf6a24ca54fea6905c7305
Muhammad Tashfeen <muhammad.tashfeen.amin@gmail.com>
2023-08-11 22:08:12 +0200

    fix: navigation

--- App.js
+++ App.js
@@ -1,39 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import AuthStack from './src/navigation/AuthStack';
-import PatientStack from './src/navigation/PatientStack';
-import SpecialistStack from './src/navigation/SpecialistStack'; 
-import { useAuth } from './src/hooks/useAuth';
-import { doc, getDoc } from 'firebase/firestore'; 
-import { db } from './config/firebaseConfig'; 
+import React from "react";
+import AuthStack from "./src/navigation/AuthStack";
+import PatientStack from "./src/navigation/PatientStack";
+import SpecialistStack from "./src/navigation/SpecialistStack";
+import { useAuth } from "./src/hooks/useAuth";
 
 const App = () => {
   const { user } = useAuth();
-  const [userType, setUserType] = useState(null);
 
-  useEffect(() => {
-    if (user) {
-      const userDocRef = doc(db, 'users', user.uid); 
-      getDoc(userDocRef)
-        .then((docSnapshot) => {
-          if (docSnapshot.exists()) {
-            const userData = docSnapshot.data();
-            setUserType(userData.userType);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching user data:', error);
-        });
-    }
-  }, [user]);
-
-  if (user) {
-    if (userType === 'patient') {
-      return <PatientStack />;
-    } else if (userType === 'specialist') {
-      return <SpecialistStack />;
-    } else {
+  if (user && user.userType === "patient") {
+    return <PatientStack />;
+  } else if (user && user.userType === "specialist") {
+    return <SpecialistStack />;
+  } else {
     return <AuthStack />;
   }
 };
-}
 export default App;
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,25 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged} from 'firebase/auth';
-import app from '../../config/firebaseConfig';
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app, { db } from "../../config/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
 
 const auth = getAuth(app);
 
 export function useAuth() {
-    const [user, setUser] = useState();
+  const [user, setUser] = useState();
 
-    useEffect(() => {
-        const unsubscribeFromAuthStatusChanged = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser(user); //user signed in
-            } else {
-                setUser(undefined); //user signout
-            }
-        });
+  useEffect(() => {
+    const unsubscribeFromAuthStatusChanged = onAuthStateChanged(
+      auth,
+      (user) => {
+        if (user) {
+          const userDocRef = doc(db, "users", user.uid);
+          getDoc(userDocRef)
+            .then((docSnapshot) => {
+              if (docSnapshot.exists()) {
+                const userData = docSnapshot.data();
+                user.name = userData.name;
+                user.userType = userData.userType;
+              }
+              setUser(user); //user signed in
+            })
+            .catch((error) => {
+              console.error("Error fetching user data:", error);
+            });
+        } else {
+          setUser(undefined); //user signout
+        }
+      }
+    );
 
-        return unsubscribeFromAuthStatusChanged;
-    }, [])
+    return unsubscribeFromAuthStatusChanged;
+  }, []);
 
-    return {
-        user,
-    };
-};
+  return {
+    user,
+  };
+}
